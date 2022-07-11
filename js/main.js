37fd1e753f8915d44b4a41cedcc2e5d94bcba2cf
Rebecca Peng <rebeccapeng@Rebeccas-MacBook-Pro-2.local>
2022-07-11 11:55:17 +0700

    fix home page blog section

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
    $(window).load(function() {
 
       // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+	   $("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
         $("#preloader").delay(300).fadeOut("slow");
